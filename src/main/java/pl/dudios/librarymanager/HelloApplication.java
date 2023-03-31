ff611219bd114a8c258a3c1960d22a47e4e921c2
Filip <92389794+w60846@users.noreply.github.com>
2023-03-31 15:09:52 +0200

    message

--- src/main/java/pl/dudios/librarymanager/HelloApplication.java
+++ src/main/java/pl/dudios/librarymanager/HelloApplication.java
@@ -13,8 +13,30 @@ public class HelloApplication extends Application {
 
         //DataBase.fillBookDataBase();
         //DataBase.fillUserDataBase();
-        //jpackage --input target --name library --main-jar library-manager-1.jar --main-class pl.dudios.librarymanager.HelloApplication --type dmg --icon bookicon.icns
-        //jpackage --input target --name library --main-jar library-manager-1.jar --main-class pl.dudios.librarymanager.HelloApplication --type exe --icon bookicon.ico
+
+        /*
+        jpackage --input target --name library --main-jar library-manager-1.jar --main-class pl.dudios.librarymanager.HelloApplication --type dmg --icon bookicon.icns --module-path /Users/dudzi/Library/Java/JavaVirtualMachines/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml,org.controlsfx.controls,net.synedra.validatorfx,org.kordamp.ikonli.javafx,org.kordamp.bootstrapfx.core,org.hibernate,org.postgresql,org.mindrot.jbcrypt,org.projectlombok.lombok-->
+        jpackage --input target --name library --main-jar library-manager-1.jar --main-class pl.dudios.librarymanager.HelloApplication --type dmg --icon bookicon.icns
+        jpackage --input target --name library --main-jar library-manager-1.jar --main-class pl.dudios.librarymanager.HelloApplication --type exe --icon bookicon.ico
+            jpackage --type dmg \
+    --input ./target/classes \
+    --name library \
+    --main-jar library-manager-1.jar  \
+    --main-class HelloApplication \
+    --java-options "--module-path /ścieżka/do/Twojego/projektu/lib:/ścieżka/do/javafx/sdk/lib" \
+    --java-options "--add-modules javafx.controls,javafx.fxml,java.persistence" \
+    --java-options "--add-opens javafx.base/com.sun.javafx.runtime=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.graphics/com.sun.javafx.stage=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.base/com.sun.javafx.binding=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.base/com.sun.javafx.event=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.graphics/com.sun.javafx.tk.quantum=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.graphics/com.sun.javafx.text=ALL-UNNAMED" \
+    --java-options "--add-opens javafx.graphics/com.sun.javafx.iio=ALL-UNNAMED"
+
+         */
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login/login-form.fxml"));
